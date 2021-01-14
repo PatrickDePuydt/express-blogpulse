cc35bf8bba4a5bf2adf03b5bbec0086ecedd39fc
PatrickDePuydt <prdepuydt@gmail.com>
2021-01-14 15:49:12 -0700

    patrickdepuydt-1547: Stable state

---
+++ scratchpad.sql
@@ -0,0 +1,11 @@
+
+sequelize model:create --name comment --attributes commentator_name:string,comment_payload:string,article:string
+
+
+
+sequelize model:create --name comment --attributes commentator_name:string,comment_payload:string,article:string
+
+
+
+sequelize model:create --name comment --attributes commentator_name:string,comment_payload:text,articleId:integer
+
