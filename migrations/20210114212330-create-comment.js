20a49b096fdd7b1bff17c95e4674e908c1a36060
PatrickDePuydt <prdepuydt@gmail.com>
2021-01-14 14:25:25 -0700

    patdepuydt: Add comment model

---
+++ migrations/20210114212330-create-comment.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('comments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      commentator_name: {
+        type: Sequelize.STRING
+      },
+      comment_payload: {
+        type: Sequelize.TEXT
+      },
+      article: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('comments');
+  }
+};
